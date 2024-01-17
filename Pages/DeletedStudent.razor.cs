267be49fc26499d3554e19516c45ca9e36bd1469
vipinv-equitec <156300849+vipinv-equitec@users.noreply.github.com>
2024-01-17 11:52:44 +0530

    init

---
+++ Pages/DeletedStudent.razor.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorApp.Pages
+{
+    public partial class DeletedStudent
+    {
+        private List<BlazorApp.Models.RestoreStudentResult>? DeletedStudents;
+
+        protected override async Task OnInitializedAsync()
+        {
+            DeletedStudents = await StudentService.GetDeletedStudents();
+        }
+
+        private async Task OnRestoreClicked(int id)
+        {
+            await StudentService.RestoreStudentAsync(id);
+            NavigationManager.NavigateTo(NavigationManager.Uri, forceLoad: true);
+        }
+        private async Task BackToList()
+        {
+            NavigationManager.NavigateTo("/studentdata");
+        }
+    }
+}
